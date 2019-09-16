879187f76778dc43f1ee6ba71c2ba43e241b6637
Dat Tran <r3curx@icloud.com>
2019-09-17 00:07:40 +0700

    layout test

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] Camera gameCamera = null;
+
+    public void ZoomOut ()
+    {
+        gameCamera.fieldOfView += 5f;
+    }
+
+    public void ZoomIn ()
+    {
+        float newFieldOfView = gameCamera.fieldOfView - 5f;
+        if (newFieldOfView < 1f)
+        {
+            newFieldOfView = 1f;
+        }
+        gameCamera.fieldOfView = newFieldOfView;
+    }
+}
