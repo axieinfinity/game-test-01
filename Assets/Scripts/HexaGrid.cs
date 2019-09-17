aa50876ce91064ecad587d24f083dc9fb5d74a9a
rec4rx <rec4rx@gmail.com>
2019-09-17 10:41:15 +0700

    change grid structure

--- Assets/Scripts/HexaGrid.cs
+++ Assets/Scripts/HexaGrid.cs
@@ -4,60 +4,55 @@ using UnityEngine;
 
 public class HexaGrid : MonoBehaviour
 {
-    [SerializeField] int battleSize = 1; //the attakers and defenders round, default by 1
-    [SerializeField] Character cyanPrefab = null; //the attacker prefab
-    [SerializeField] Character greenPerfab = null; // the defender one
+    [SerializeField] int gridSize = 1;
+    [SerializeField] HexaUnit cyanPrefab = null;
+    [SerializeField] HexaUnit greenPrefab = null;
 
-    const int HEXA_GRID_CONSTANT = 6;
-    const float HEXA_RADIUS_CONSTANT = 0.75f;
+    private const int HEXA_GRID_CONSTANT = 6;
+    private const float HEXA_RADIUS_CONSTANT = 0.75f;
     Vector2 CENTER_POINT = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    private List<HexaUnit> units = new List<HexaUnit>();
+    
+    private void Start()
     {
         // init defenders
-        for (int i = 0; i <= battleSize; i++)
+        for (int i = 0; i <= gridSize; i++)
         {
             int radius = i;
             int numberOfElement = radius == 0 ? 1 : radius * HEXA_GRID_CONSTANT;
 
             for (int j = 0; j < numberOfElement; j++)
             {
-                Character greenChar = Instantiate(greenPerfab, transform);
+                HexaUnit hexaUnit = Instantiate(greenPrefab, transform);
                 int idx = j % numberOfElement;
                 float rotation = idx * (360f / numberOfElement);
                 float positionX = CENTER_POINT.x + radius * HEXA_RADIUS_CONSTANT * Mathf.Cos(Mathf.Deg2Rad * rotation);
                 float positionY = CENTER_POINT.y + radius * HEXA_RADIUS_CONSTANT * Mathf.Sin(Mathf.Deg2Rad * rotation);
-                greenChar.gameObject.SetActive(true);
-                greenChar.transform.localPosition = new Vector3(positionX, positionY, 0);
-            }  
+                hexaUnit.gameObject.SetActive(true);
+                hexaUnit.transform.localPosition = new Vector3(positionX, positionY, 0);
+            }
         }
 
         //init attackers
         {
-            for (int i = 0; i <= battleSize; i++)
+            for (int i = 0; i <= gridSize; i++)
             {
-                int radius = battleSize + i + 2;
+                int radius = gridSize + i + 2;
                 int numberOfElement = radius == 0 ? 1 : radius * HEXA_GRID_CONSTANT;
 
                 for (int j = 0; j < numberOfElement; j++)
                 {
-                    Character cyanChar = Instantiate(cyanPrefab, transform);
+                    HexaUnit hexaUnit = Instantiate(cyanPrefab, transform);
                     int idx = j % numberOfElement;
                     float rotation = idx * (360f / numberOfElement);
                     float positionX = CENTER_POINT.x + radius * HEXA_RADIUS_CONSTANT * Mathf.Cos(Mathf.Deg2Rad * rotation);
                     float positionY = CENTER_POINT.y + radius * HEXA_RADIUS_CONSTANT * Mathf.Sin(Mathf.Deg2Rad * rotation);
-                    cyanChar.gameObject.SetActive(true);
-                    cyanChar.transform.localPosition = new Vector3(positionX, positionY, 0);
+                    hexaUnit.gameObject.SetActive(true);
+                    hexaUnit.transform.localPosition = new Vector3(positionX, positionY, 0);
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
  
